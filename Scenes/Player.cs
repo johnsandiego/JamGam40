ef8b065aa1ab4a357335eb35453c122c1d81f9ec
JOHN SAN DIEGO <john.sandiego@tylertech.com>
2024-05-23 21:13:50 -0400

    added some juice during hit
    - added a help menu for control
    - added player 1 and 2 multiplier on hit so every hit the force will multiply
    - added triple jump

--- Scenes/Player.cs
+++ Scenes/Player.cs
@@ -5,31 +5,53 @@ public partial class Player : CharacterBody2D
 {
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
+    public int playerMultiplier = 0;
 
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
-	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+    // Get the gravity from the project settings to be synced with RigidBody nodes.
+    public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
     [Export]
     public PackedScene NeedleScene;
     [Export]
     public Node2D SpawnPoint;
 
+    private float impulse = 1f;
+    public AnimationPlayer hitFlash;
+    private int tripleJump = 0;
+
     public override void _Ready()
     {
         SpawnPoint = GetNode<Node2D>("spawnPoint");
-        base._Ready();
+        hitFlash = GetNode<AnimationPlayer>("AnimationPlayer");
+
     }
 
     public override void _PhysicsProcess(double delta)
-	{
-		Vector2 velocity = Velocity;
+    {
+        Vector2 velocity = Velocity;
 
-		// Add the gravity.
-		if (!IsOnFloor())
-			velocity.Y += gravity * (float)delta;
+        // Add the gravity.
+        if (!IsOnFloor())
+        {
+            velocity.Y += gravity * (float)delta;
+        }
+        else
+        {
+            tripleJump = 0;
+        }
 
-		// Handle Jump.
-		if (Input.IsActionJustPressed("player2_up") && IsOnFloor())
-			velocity.Y = JumpVelocity;
+        // Handle Jump.
+        if (Input.IsActionJustPressed("player2_up") )
+        {
+            if (IsOnFloor() || tripleJump <= 3)
+            {
+                velocity.Y = JumpVelocity;
+                tripleJump++;
+                GD.Print(tripleJump);
+
+            }
+
+        }
+
 
         // Get the input direction and handle the movement/deceleration.
         // As good practice, you should replace UI actions with custom gameplay actions.
@@ -55,14 +77,24 @@ public partial class Player : CharacterBody2D
 		MoveAndSlide();
 	}
 
+    public void OnBodyEntered(Node2D body)
+    {
+        if (body != null && body.IsInGroup("player2"))
+        {
+            hitFlash.Play("juice");
+            playerMultiplier += 5;
+        }
+    }
+
 
     private void LaunchBullet()
     {
         Needle needleInstance = (Needle)NeedleScene.Instantiate();
+        needleInstance.AddToGroup("player1");
         AddChild(needleInstance);
 
         // Set the bullet's position to the player's position or the firing point.
-        needleInstance.Position = GlobalPosition + new Vector2(-200, 0);
+        needleInstance.Position = GlobalPosition + new Vector2(500, 0);
 
         // Set the bullet's direction based on the player's current rotation.
         needleInstance.Direction = new Vector2(Mathf.Cos(Rotation), Mathf.Sin(Rotation)).Normalized();
--- Scripts/Manager.cs
+++ Scripts/Manager.cs
@@ -3,13 +3,31 @@ using System;
 
 public partial class Manager : Node
 {
+	[Export]
+	public Panel helpMenu;
+
+	public Player player;
+	public Player2 player2;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-	}
+		player = GetNode<Player>("Necromancer");
+        player2 = GetNode<Player2>("Necromancer2");
+
+    }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		
 	}
+
+    public override void _Input(InputEvent @event)
+    {
+        if (Input.IsKeyPressed(Key.H))
+        {
+            helpMenu.Visible = !helpMenu.Visible;
+
+        }
+    }
 }
